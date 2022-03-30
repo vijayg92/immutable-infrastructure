dee783439e30f74ef8cb2e396819eae9304c83d9
Vijay Gosai <singhgos@adobe.com>
2022-03-30 11:42:16 +0530

    Added Packer

---
+++ packer/provisioner/webserver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+function configure_infra() {
+  yum update -y
+  yum install -y git python3 python3-pip
+  test -d flask-hello-world || git clone https://github.com/vijayg92/flask-hello-world
+  pip3 install -r flask-hello-world/requirements.txt
+  nohup python3 flask-hello-world/hello.py &
+  curl -I http://$(hostname -i)
+}
+
+function validate_infra() {
+  gem --version || yum install -y gem
+  sudo gem install bundler --no-ri --no-rdoc
+  cd /tmp/serverspec
+  bundle install --path=vendor
+  bundle exec rake spec
+}
+
+### Main Function - Call ##
+configure_infra
+validate_infra
